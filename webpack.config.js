a5d150b8d79ce374fd20c5f9e5a2b1dd576ddcde
Changuk Choi <devchoi1213@gmail.com>
2023-10-30 00:06:06 +0900

    Output Management - Cleaning up the /dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -16,5 +16,6 @@ module.exports = {
 		// 출력 번들 이름이 엔트리 포인트 이름을 기반으로 동적으로 생성
 		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist'),
+		clean: true,	// 빌드에서 생성된 파일만 남기고 dist 폴더 정리
 	},
 };
