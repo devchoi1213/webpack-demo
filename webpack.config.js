3850a65751e751ea4dea39618c137e5699d06f2b
Changuk Choi <devchoi1213@gmail.com>
2023-10-30 00:04:46 +0900

    Output Management - Setting HtmlWebPackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: {
 		index: './src/index.js',
 		print: './src/print.js',
 	},
+	// index.html 파일을 output 폴더에 생성 (모든 번들이 자동으로 추가)
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Output Management',
+		}),
+	],
 	output: {
 		// 출력 번들 이름이 엔트리 포인트 이름을 기반으로 동적으로 생성
 		filename: '[name].bundle.js',
