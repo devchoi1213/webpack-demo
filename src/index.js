e4b3c6e5588b9cef35363e898b47f9d6ea218c7f
Changuk Choi <devchoi1213@gmail.com>
2023-10-30 00:00:31 +0900

    Output Management - Dynamic Bundle Name

--- src/index.js
+++ src/index.js
@@ -1,27 +1,17 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.jpg';	// 이미지의 최종 URL을 포함
-import Data from './data.xml';
-import Notes from './data.csv';
+import printMe from './print.js';
 
 function component() {
 	const element = document.createElement('div');
+	const btn = document.createElement('button');
 
 	// 이 라인이 동작하려면 현재 스크립트를 통해 포함된 Lodash가 필요합니다.
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-	// 요소의 클래스명 리스트를 반환 (DOMTokenList)
-	// 요소의 클래스명 리스트에 hello를 추가
-	element.classList.add('hello');
+	btn.innerHTML = 'Click me and check the console!';
+	btn.onclick = printMe;
 
-	// 원래 있던 div 에 이미지를 추가합니다.
-	const myIcon = new Image();
-	myIcon.src = Icon;
-
-	element.appendChild(myIcon);
-
-	console.log(Data);
-	console.log(Notes);
+	element.appendChild(btn);
 
 	return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,13 @@
 const path = require('path');
 
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist'),
+	entry: {
+		index: './src/index.js',
+		print: './src/print.js',
 	},
-	// 모듈 로더는 체인으로 연결
-	// 체인은 역순으로 실행
-	// 아래 로더 순서는 유지되어야 함
-	// .css로 끝나는 모든 파일이 style-loader와 css-loader에 전달
-	module: {
-		rules: [
-			{
-				test: /\.css$/i,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
-				test: /\.(png|svg|jpg|jpeg|gif)$/i,
-				type: 'asset/resource',
-			},
-			{
-				test: /\.(woff|woff2|eot|ttf|otf)$/i,
-				type: 'asset/resource',
-			},
-			{
-				test: /\.(csv|tsv)$/i,
-				use: ['csv-loader'],
-			},
-			{
-				test: /\.xml$/i,
-				use: ['xml-loader'],
-			},
-		],
+	output: {
+		// 출력 번들 이름이 엔트리 포인트 이름을 기반으로 동적으로 생성
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist'),
 	},
 };
