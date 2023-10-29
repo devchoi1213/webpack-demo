3e2984b8c3bcefd4720f130d14cb8f3114dadfaf
Changuk Choi <devchoi1213@gmail.com>
2023-10-29 23:45:48 +0900

    Asset Management - Loading Images

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './icon.jpg';	// 이미지의 최종 URL을 포함
 
 function component() {
 	const element = document.createElement('div');
@@ -10,6 +11,13 @@ function component() {
 	// 요소의 클래스명 리스트를 반환 (DOMTokenList)
 	// 요소의 클래스명 리스트에 hello를 추가
 	element.classList.add('hello');
+
+	// 원래 있던 div 에 이미지를 추가합니다.
+	const myIcon = new Image();
+	myIcon.src = Icon;
+
+	element.appendChild(myIcon);
+
 
 	return element;
 }
